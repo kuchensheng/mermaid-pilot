2da3c30fe5a4e28bfaef8a50272fe7797d0aaf84
库陈胜 <xiongm@isyscore.com>
2022-01-30 13:32:18 +0800

    添加文件监听，收集日志

--- mermaid-pilot-agent/src/main/java/org/mermaid/pilog/agent/common/CommandConfig.kt
+++ mermaid-pilot-agent/src/main/java/org/mermaid/pilog/agent/common/CommandConfig.kt
@@ -1,5 +1,22 @@
 package org.mermaid.pilog.agent.common
 
+import com.sun.nio.file.SensitivityWatchEventModifier
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import net.sf.json.JSONObject
+import okio.ByteString.Companion.toByteString
+import org.apache.commons.io.monitor.FileAlterationListenerAdaptor
+import org.apache.commons.io.monitor.FileAlterationMonitor
+import org.apache.commons.io.monitor.FileAlterationObserver
+import org.mermaid.pilog.agent.advice.LogInfo
+import java.io.File
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+import java.nio.file.*
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
 fun readCommandLineArgs(args: List<String>?) {
     println("命令行参数，args = $args,size = ${args?.size}")
     args?.forEach { arg ->
@@ -10,9 +27,76 @@ fun readCommandLineArgs(args: List<String>?) {
                 "report.service.url" -> CommandConfig.serviceUri = it[1]
                 "report.service.type" -> it[1]?.let { v ->  CommandConfig.reportType = ReportType.valueOf(v) }
                 "spring.application.name" -> CommandConfig.appName = it[1]
+                "log.path" -> logFileWatcher(it[1])
             }
         }
     }
+}
+
+private fun logFileWatcher(dir : String) {
+    println("监听日志文件目录:$dir")
+    val watchService = FileSystems.getDefault().newWatchService()
+    Paths.get(dir).register(watchService, arrayOf(StandardWatchEventKinds.ENTRY_MODIFY),SensitivityWatchEventModifier.HIGH)
+    var lastPosition = 0L
+    CoroutineScope(Dispatchers.IO).launch {
+        while (true) {
+            //文件监听,获取不到事件则等待
+            val take = watchService.take()
+            //获取当前时间戳
+            val now = LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli()
+            val event = take.pollEvents()[0]
+            //读取监听Path
+            val path = com.sun.jmx.mbeanserver.Util.cast<Path>(event.context())
+            println("path=$path")
+            //只关注目标文件
+            val fullPath = Paths.get(dir, path.toString())
+            println("fullPath=$fullPath")
+            //目标文件最新更新事件
+            val lastModifiedTime = Files.getLastModifiedTime(fullPath).toMillis()
+            //更新事件超过1s钟的文件内容则放弃读取
+            if (now - lastModifiedTime > 1000) {
+                lastPosition = Files.size(fullPath)
+                continue
+            }
+            println("读取文件内容path=$fullPath,lastPosition=$lastPosition")
+            try {
+                val channel = Files.newByteChannel(fullPath, StandardOpenOption.DSYNC)
+                //增量读取文件内容
+                val buffer = ByteBuffer.allocateDirect(65536)
+                channel.use {
+                    it.position(lastPosition).read(buffer)
+                    lastPosition = it.position()
+                }
+                val c = buffer.toByteString().toString()
+                println("开始日志上报,content=$c")
+                //日志上报
+                val logInfo = LogInfo().apply {
+                    tags["kucs"] = "watcher"
+                    content = c
+                }
+                println("日志内容:${JSONObject.fromObject(logInfo).toString()}")
+                blockingQueue.add(logInfo)
+            } catch (e: Exception) {
+                e.stackTrace
+            }
+        }
+    }
+
+//    val observer = FileAlterationObserver(dir)
+//    val adaptor = object : FileAlterationListenerAdaptor() {
+//        override fun onFileChange(file: File) {
+//            super.onFileChange(file)
+//        }
+//    }
+//    //注册监听器
+//    observer.addListener(adaptor)
+//    //启动监听
+//    val monitor = FileAlterationMonitor().apply { addObserver(observer) }
+//    //启动协程
+//    CoroutineScope(Dispatchers.IO).launch {
+//        monitor.start()
+//    }
+
 }
 
 object CommandConfig {
